371da540ae6e372906f6040f5ef66f242eaef451
ssbdragonfly <ssbdragonfly@gmail.com>
2025-01-25 03:29:16 -0500

    hopefully fix

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ export default function App() {
                     <Analysis />
                   </SignedIn>
                   <SignedOut>
-                    <RedirectToSignIn />
+                    <RedirectToSignIn redirectUrl="/analysis" />
                   </SignedOut>
                 </>
               }
